3c1b18bb21081496535e349b4cf36ba0b71fdb95
builtbyedgar <eledgaar@gmail.com>
2026-01-30 08:37:00 +0100

    Initial Commit

---
+++ FleyPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FleyPlanner
+//
+//  Created by Edgar Bermejo on 30/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
